484415a94a9262ee166b8bbdfd132b507253050c
fedelmar <fsuarez91@gmail.com>
2022-02-14 19:44:47 -0300

    remove await

--- client.js
+++ client.js
@@ -1,29 +1,22 @@
 const redis = require("redis");
 
 
-let client;
-(async () => {
-  try {
-    const config = {
-      host: 'localhost',
-      port: 6379,
-    };
-    client = await redis.createClient(config);
+const config = {
+  host: 'localhost',
+  port: 6379,
+};
+const client = await redis.createClient(config);
 
-    client.on('connect', () => console.log('Redis Client connect'));
-    client.on('ready', () => console.log('Redis Client ready'));
-    client.on('end', () => console.log('Redis Client end'));
-    client.on('error', (err) => console.log('Redis Client Error', err));
-    client.on('reconnecting', () => console.log('Redis Client reconnecting'));
+client.on('connect', () => console.log('Redis Client connect'));
+client.on('ready', () => console.log('Redis Client ready'));
+client.on('end', () => console.log('Redis Client end'));
+client.on('error', (err) => console.log('Redis Client Error', err));
+client.on('reconnecting', () => console.log('Redis Client reconnecting'));
 
-    await client.connect();
-    console.log(client)
-    // Establece la clave "octocat" para un valor de "Mona the octocat"
-    client.set("octocat", "Mona the Octocat", redis.print);
-  } catch (e) {
-    console.log(e);
-  }
-})();
+client.connect();
+console.log(client)
+// Establece la clave "octocat" para un valor de "Mona the octocat"
+client.set("octocat", "Mona the Octocat", redis.print);
 
 
 module.exports = {
