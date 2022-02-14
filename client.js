ecf3daf7ff3fa219d0394bcbb87ad8bbfb137018
fedelmar <fsuarez91@gmail.com>
2022-02-14 19:27:27 -0300

    fix client

--- client.js
+++ client.js
@@ -3,31 +3,19 @@ const redis = require("redis");
 // Crea un nuevo cliente Redis
 // Si no se establece REDIS_HOST, el host predeterminado es localhost
 // Si no se establece REDIS_PORT, el puerto predeterminado es 6379
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT  
+});
 
-let redisClient;
-(async () => {
-  try {
-    const redisClient = await redis.createClient({
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT  
-    });
+redisClient.on("error", function(err) {
+    console.log("Error " + err);
+});
 
-    redisClient.on("error", function(err) {
-      console.log("Error " + err);
-     });
-    await redisClient.connect();
+redisClient.connect();
 
-    // Establece la clave "octocat" para un valor de "Mona the octocat"
-    redisClient.set("octocat", "Mona the Octocat", redis.print);
-
-  } catch (e) {
-    console.log(e);
-  }
-})();
-
-
-
-
+// Establece la clave "octocat" para un valor de "Mona the octocat"
+redisClient.set("octocat", "Mona the Octocat", redis.print);
 
 module.exports = {
   redisClient,
